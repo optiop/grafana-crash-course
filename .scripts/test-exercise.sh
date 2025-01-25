e3c5563db396d629df0377dbff611e8fe40f25e0
Mehrshad Lotfi <mehrshad.lotfi@optiop.org>
2025-01-25 09:19:51 +0100

    Use linter to fix bash lint issues

--- .scripts/test-exercise.sh
+++ .scripts/test-exercise.sh
@@ -7,25 +7,23 @@ echo ""
 # Set print command
 PS4='\033[34m>> \033[0m'
 
+for dir in $lab; do
+	if [ -d "$dir" ]; then
+		echo "#!/bin/bash" >"$dir/install.sh"
+		echo "source .scripts/test-utils.sh" >>"$dir/install.sh"
+		awk '/lab-instruction/ {found=1} found' "$dir/README.md" | sed -n '/```/,/```/p' | sed '/```/d' >>$dir/install.sh
+		echo "----------------------------------------------------------------------------------------------------"
+		cat "$dir/install.sh"
+		echo "----------------------------------------------------------------------------------------------------"
+		chmod +x "$dir/install.sh"
+		echo "Running $dir/install.sh"
 
+		# Run the install.sh script
+		set -x
+		"$dir/install.sh"
+		set +x
 
-for dir in $lab; do \
-  if [ -d "$dir" ]; then \
-    echo "#!/bin/bash" > "$dir/install.sh"; \
-    echo "source .scripts/test-utils.sh" >> "$dir/install.sh"; \
-    awk '/lab-instruction/ {found=1} found' "$dir/README.md" | sed -n '/```/,/```/p' | sed '/```/d' >> $dir/install.sh; \
-    echo "----------------------------------------------------------------------------------------------------"; \
-    cat "$dir/install.sh"; \
-    echo "----------------------------------------------------------------------------------------------------"; \
-    chmod +x "$dir/install.sh"; \
-    echo "Running $dir/install.sh"; \
-
-    # Run the install.sh script
-    set -x
-    "$dir/install.sh"; \
-    set +x
-
-    rm -rf "$dir/install.sh"; \
-    echo "Test completed for $dir/"; \
-  fi \
+		rm -rf "$dir/install.sh"
+		echo "Test completed for $dir/"
+	fi
 done
--- .scripts/test-utils.sh
+++ .scripts/test-utils.sh
@@ -1,17 +1,17 @@
 # Check if a URL is reachable via curl
 check_reachable_via_curl() {
-  local url=$1
-  local regex=$2
+	local url=$1
+	local regex=$2
 
-  local retry=3
-  local delay=4
-  echo "  [INFO] Attempting to reach URL: $url and match regex: $regex"
-  for i in $(seq 1 $retry); do \
-    curl -s $url | grep -q "$regex" \
-      && echo "  [PASSED] URL: $url is reachable and matches the regex: $regex" \
-      && return 0; \
-    sleep $delay; \
-  done 
-  echo "  [FAILED] URL: $url is not reachable or does not match the regex: $regex"
-  return 1
+	local retry=3
+	local delay=4
+	echo "  [INFO] Attempting to reach URL: $url and match regex: $regex"
+	for i in $(seq 1 $retry); do
+		curl -s $url | grep -q "$regex" &&
+			echo "  [PASSED] URL: $url is reachable and matches the regex: $regex" &&
+			return 0
+		sleep $delay
+	done
+	echo "  [FAILED] URL: $url is not reachable or does not match the regex: $regex"
+	return 1
 }

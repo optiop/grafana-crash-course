4ce9b32103cd5b4fec4be4de772d70e0a8dee342
Mehrshad Lotfi <mehrshad.lotfi@optiop.org>
2025-01-25 09:13:11 +0100

    Fix lint issue with bash

--- .scripts/test-exercise.sh
+++ .scripts/test-exercise.sh
@@ -13,7 +13,7 @@ for dir in $lab; do \
   if [ -d "$dir" ]; then \
     echo "#!/bin/bash" > "$dir/install.sh"; \
     echo "source .scripts/test-utils.sh" >> "$dir/install.sh"; \
-    awk '/lab-instruction/ {found=1} found' $dir/README.md | sed -n '/```/,/```/p' | sed '/```/d' >> $dir/install.sh; \
+    awk '/lab-instruction/ {found=1} found' "$dir/README.md" | sed -n '/```/,/```/p' | sed '/```/d' >> $dir/install.sh; \
     echo "----------------------------------------------------------------------------------------------------"; \
     cat "$dir/install.sh"; \
     echo "----------------------------------------------------------------------------------------------------"; \
@@ -22,7 +22,7 @@ for dir in $lab; do \
 
     # Run the install.sh script
     set -x
-    $dir/install.sh; \
+    "$dir/install.sh"; \
     set +x
 
     rm -rf "$dir/install.sh"; \

64c39b08a2f7b0b01cdf28063a85cd8ea83d2105
Arian Delavarjou <mr.arian.delavarjou@gmail.com>
2025-02-04 10:48:54 +0330

    add farshad reviews to code

--- service_testing/internal/utils/loki_logs.go
+++ service_testing/internal/utils/loki_logs.go
@@ -1,9 +1,16 @@
 package utils
 
-import "context"
+import (
+	"context"
+	"fmt"
+	"net/url"
+)
 
 func GetLokiLogsExist(ctx context.Context, lokiURL string) (string, error) {
-	res, err := RequestGetAddr(ctx, lokiURL, 200, `/loki/api/v1/query --data-urlencode 'query=sum(rate({job="varlogs"}[10m])) by (level)' | jq`)
+	query := `sum(rate({job="varlogs"}[10m])) by (level)`
+	url := fmt.Sprintf("%s/loki/api/v1/query?query=%s", lokiURL, url.QueryEscape(query))
+
+	res, err := RequestGetAddr(ctx, url, 200)
 	if err != nil {
 		return "", err
 	}

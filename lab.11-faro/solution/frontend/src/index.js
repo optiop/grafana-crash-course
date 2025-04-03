8562a19e497723bd5340ac7399d97d592a303b8c
afragerami <afragerami@gmail.com>
2025-04-03 13:02:11 +0330

    config frontend with faro

--- lab.11-faro/solution/frontend/src/index.js
+++ lab.11-faro/solution/frontend/src/index.js
@@ -1,9 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import {
+  getWebInstrumentation,
+  initializeFaro,
+  ReactIntegration,
+  ReactRouterVersion,
+} from "@grafana/faro-react";
+import { TracingInstrumentation } from "@grafana/faro-web-tracing";
+import {
+  createRoutesFromChildren,
+  matchRoutes,
+  Routes,
+  useLocation,
+  useNavigationType,
+} from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+initializeFaro({
+  url: "https://faro.fo11y.com",
+  app: {
+    name: "fo11y-demo",
+    version: "1.0.0",
+    environment: "production",
+  },
+
+  instrumentations: [
+    ...getWebInstrumentation(),
+
+    new TracingInstrumentation(),
+
+    new ReactIntegration({
+      router: {
+        version: ReactRouterVersion.V6,
+        dependencies: {
+          createRoutesFromChildren,
+          matchRoutes,
+          Routes,
+          useLocation,
+          useNavigationType,
+        },
+      },
+    }),
+  ],
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />

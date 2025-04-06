9419a68beef9c7f33358392265e5c74f7e7ed5dd
Amir Mahdi Khazaee <amir.mahdi.khazeee@gmail.com>
2025-04-06 13:08:35 +0330

    Delete unnecessary go files (#39)

--- lab.01.5-webhook-whatsapp/solution/webhook/interfaces/whatsapp.go
+++
@@ -1,8 +0,0 @@
-package interfaces
-
-import "whatsapp_service/entity"
-
-type WhatsappService interface {
-	SendNewWhatsAppMessageToUser(msg entity.Message)
-	SendNewWhatsAppMessageToGroup(msg entity.Message)
-}

0a21ca90af269bc271656ced8b402f9a92057f49
Arian Delavarjou <mr.arian.delavarjou@gmail.com>
2025-04-14 17:18:03 +0330

    latest changes

--- lab.19-kind-loki-grafana/solution/setup.sh
+++ lab.19-kind-loki-grafana/solution/setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-# Kill running process 
+# Kill running process
 lsof -ti:3000 | xargs kill -9 || echo "No process found on port 3000"
 lsof -ti:12345 | xargs kill -9 || echo "No process found on port 12345"
-lsof -ti:3100 | xargs kill -9 || echo "No process found on port 12345"
+lsof -ti:3100 | xargs kill -9 || echo "No process found on port 3100"
 
-# run kind cluster
+# Run kind cluster
 CLUSTER_NAME=$(basename "$(dirname "$(pwd)")")
-kind delete cluster --name $CLUSTER_NAME
-kind create cluster --name $CLUSTER_NAME
+kind delete cluster --name $CLUSTER_NAME --config kind-config.yaml
+kind create cluster --name $CLUSTER_NAME --config kind-config.yaml
 
 # Add Helm repo
 helm repo add grafana https://grafana.github.io/helm-charts
@@ -18,10 +18,10 @@ helm upgrade --install alloy --namespace alloy --create-namespace grafana/alloy 
 helm upgrade --install loki --namespace loki --create-namespace grafana/loki -f ./loki/values.yaml
 
 # Port-forwarding
-sleep 10
-kubectl port-forward -n grafana service/grafana 3000:80 2>&1 > /dev/null &
+echo "Forwarding ports..."
+kubectl port-forward -n grafana service/grafana 3000:3000 2>&1 > /dev/null &
 kubectl port-forward -n alloy service/alloy 12345:12345 2>&1 > /dev/null &
-kubectl port-forward -n loki service/loki-write 3100:3100 2>&1 > /dev/null &
+kubectl port-forward -n loki service/loki-gateway 3100:3100 2>&1 > /dev/null &
 
 # Retrieve Grafana admin password
 GRAFANA_SECRET=$(kubectl get secrets -n grafana grafana -o jsonpath={'.data.admin-password'} | base64 -d)
